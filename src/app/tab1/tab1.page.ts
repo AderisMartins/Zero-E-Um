1f3720708eb197b833480b50ba21c86c665d5b88
AderisMartins <aderismartins.f@gmail.com>
2019-08-02 04:33:03 -0300

    up

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+//public lista_habilidades = ['Html5', 'Css3', 'Javascript', 'Angular', 'Java', 'Hibernate', 'JPA', 'Pl/Sql', 'Git', 'Svn', 'Linux'];
+
+  constructor() {}
+
+}
